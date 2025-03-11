8a0187cb0255d83478b5e3820e286513ee957e0b
i-Nevsky <avptema@gmail.com>
2025-03-12 02:10:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -64,6 +64,11 @@ def webhook():
     dispatcher.process_update(update)
     return "ok", 200
 
+# Добавляем обработчик для корневого URL
+@app.route('/')
+def index():
+    return "Сервис Telegram бота работает"
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
